bcbac1fa1384d13b4a86f1589a1da50595a8d0c7
anhern <andrea.hernandez@daimler.com>
2021-09-09 10:50:09 +0200

    test-merge: edit cat

--- src/main/java/com/excersise/entities/model/Cat.java
+++ src/main/java/com/excersise/entities/model/Cat.java
@@ -7,4 +7,19 @@ public class Cat {
     private String type;
     private String owner;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
