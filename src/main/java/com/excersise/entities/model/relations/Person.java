b08863c78f9268636176f46ccc336ef0e3add3a8
anhern <andrea.hernandez@daimler.com>
2021-09-09 10:53:14 +0200

    main: remove person code

--- src/main/java/com/excersise/entities/model/relations/Person.java
+++ src/main/java/com/excersise/entities/model/relations/Person.java
@@ -1,28 +1,16 @@
 package com.excersise.entities.model.relations;
 
-import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
-import javax.persistence.OneToMany;
 import lombok.Data;
 
-/*@Data
+@Data
 @Entity
 public class Person {
   @Id
   @GeneratedValue
   private Long id;
 
-  @ManyToMany
-  @JoinTable(
-      name = "person_outfit",
-      joinColumns = { @JoinColumn(name = "person_id")},
-      inverseJoinColumns = { @JoinColumn(name = "outfit_id")}
-  )
-  private List<Outfit> outfits;
-
-}*/
+}
