d9393c2e5680e75b1a564d241aaecb6fb8767e71
anhern <andrea.hernandez@daimler.com>
2021-09-09 10:50:16 +0200

    test-merge: edit dog

--- src/main/java/com/excersise/entities/model/relations/Dog.java
+++ src/main/java/com/excersise/entities/model/relations/Dog.java
@@ -4,4 +4,21 @@ public class Dog {
 
     private String name;
     private String name2;
+    private String name3;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName2() {
+        return name2;
+    }
+
+    public void setName2(String name2) {
+        this.name2 = name2;
+    }
 }
