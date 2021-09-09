651c1949a35748d3c6c6a76ae366208fecdb49f1
anhern <andrea.hernandez@daimler.com>
2021-09-09 10:53:38 +0200

    main: remove person DataEntity

--- src/main/java/com/excersise/entities/model/relations/Person.java
+++ src/main/java/com/excersise/entities/model/relations/Person.java
@@ -1,16 +1,8 @@
 package com.excersise.entities.model.relations;
 
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import lombok.Data;
-
-@Data
-@Entity
 public class Person {
-  @Id
-  @GeneratedValue
+
   private Long id;
 
 }
