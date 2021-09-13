6c073f52cafcdefe90b7fca07fd837cd5710294d
anhern <andrea.hernandez@daimler.com>
2021-09-13 12:00:30 +0200

    main: add person get/setters

--- src/main/java/com/excersise/entities/model/relations/Person.java
+++ src/main/java/com/excersise/entities/model/relations/Person.java
@@ -7,4 +7,27 @@ public class Person {
   private int age;
   private String name;
 
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
 }
