364acb6bb5c7011910afed18b16a7ecd1e21a88c
anhern <andrea.hernandez@daimler.com>
2021-09-13 12:27:11 +0200

    main: add Cat get/setters

--- src/main/java/com/excersise/entities/model/Cat.java
+++ src/main/java/com/excersise/entities/model/Cat.java
@@ -39,4 +39,12 @@ public class Cat {
     public void setOwner(String owner) {
         this.owner = owner;
     }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
 }
