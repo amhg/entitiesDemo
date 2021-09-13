8545f31168673447dc5070cf9f6984474a0c8ab9
anhern <andrea.hernandez@daimler.com>
2021-09-13 12:01:54 +0200

    main: update Flower

--- src/main/java/com/excersise/entities/model/Flower.java
+++ src/main/java/com/excersise/entities/model/Flower.java
@@ -1,15 +1,27 @@
 package com.excersise.entities.model;
 
-import javax.persistence.Entity;
-import lombok.Data;
 
-@Data
-@Entity
 public class Flower extends Plant {
   private String color;
-  
+
   private String name;
   private String origin;
   private int qty;
   private int qty2;
+
+  public String getColor() {
+    return color;
+  }
+
+  public void setColor(String color) {
+    this.color = color;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
 }
