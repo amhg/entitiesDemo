55f2525a815dd15828f28fad9fead500331baf3b
anhern <andrea.hernandez@daimler.com>
2021-09-13 11:57:24 +0200

    test-reb2: add cat get/setter

--- src/main/java/com/excersise/entities/model/Cat.java
+++ src/main/java/com/excersise/entities/model/Cat.java
@@ -22,4 +22,20 @@ public class Cat {
     public void setAge(int age) {
         this.age = age;
     }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
 }
