9139cbbf74767008d135a757716a5530e3ac7fc3
anhern <andrea.hernandez@daimler.com>
2022-02-12 19:21:10 +0100

    branch-1:removed redundant code from Dog

--- src/main/java/com/excersise/entities/model/relations/Dog.java
+++ src/main/java/com/excersise/entities/model/relations/Dog.java
@@ -3,9 +3,6 @@ package com.excersise.entities.model.relations;
 public class Dog {
 
     private String name;
-    private String name2;
-    private String name3;
-    private String name4;
 
     public String getName() {
         return name;
@@ -15,19 +12,4 @@ public class Dog {
         this.name = name;
     }
 
-    public String getName2() {
-        return name2;
-    }
-
-    public void setName2(String name2) {
-        this.name2 = name2;
-    }
-
-    public String getName4() {
-        return name4;
-    }
-
-    public void setName4(String name4) {
-        this.name4 = name4;
-    }
 }
