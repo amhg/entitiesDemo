53424b89c08da8fe72ca32ae2cf7b8f83f541750
anhern <andrea.hernandez@daimler.com>
2022-02-22 14:01:23 +0100

    Revert "branch-1:removed values from Cat"
    This reverts commit b945a81a95c922e347af16f7a5491f00f498a375.

--- src/main/java/com/excersise/entities/model/Cat.java
+++ src/main/java/com/excersise/entities/model/Cat.java
@@ -15,5 +15,28 @@ public class Cat {
         this.name = name;
     }
 
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
 
 }
